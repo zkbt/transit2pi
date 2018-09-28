2f4a0adfd46dec0e94ef99d1983088792f5bc20a
TJ <tj@engr2-2-93-105-dhcp.int.colorado.edu>
2018-09-28 14:58:23 -0600

    New py file

---
+++ maketransitcurve2.py
@@ -0,0 +1,13 @@
+import maketransitcurve as mtc
+import planetplotlib as ppl
+import henrietta as hsl
+
+lc = hsl.download_kepler_lc('Kepler-19')  # Here import other Kepler planet data
+flattened = lc.flatten()
+folden= flattened.fold(period=9.28716)
+folden.scatter()
+binned = folden.bin(25)
+
+fig , ax  = ppl.create_dome_plot()
+binned = folden.bin(25)
+binned.scatter(ax=ax)
