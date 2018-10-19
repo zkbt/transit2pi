3f5317a3188577a3b5f60d6880c9762050e35100
Zach Berta-Thompson <zkbt@mit.edu>
2018-10-19 14:36:25 -0600

    add some tests

---
+++ test_awesome.py
@@ -0,0 +1,11 @@
+'''
+This tests test_360movie.py!
+'''
+
+
+import test_360movie
+
+p = 9.0
+t = 0.0
+lc = hsl.simulate_transit_data(period=p, t0=t)
+test_360movie.awesome_function(lc, period=p, t0=t)
---
+++ test_m2p.py
@@ -0,0 +1,7 @@
+'''
+This tests make2pitransit_2.py!
+'''
+
+
+import make2pitransit_2 as m2p
+m2p.make_transit('Kepler-10b')
