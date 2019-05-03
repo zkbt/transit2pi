473b82f25c6c7996f65b08571fdea7784530ade2
Zach Berta-Thompson <zkbt@mit.edu>
2019-05-03 14:40:18 -0600

    reorganize

--- tests/test_lightcurves.py
+++
@@ -1,5 +0,0 @@
-import henrietta as hsl
-from somefile import taylors_awesome_function
-lc = hsl.simulate_transit_data(period=1.2, t0=0.5)
-# this function makes a (flattened, folded, binned) planetarium light curve plot
-taylors_awesome_function(lc, period=1.2, t0=0.5)
